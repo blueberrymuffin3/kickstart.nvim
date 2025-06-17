e85ed4a7d8c53f2360013281f788facb3afec27d
Evelyn Gordon <evelyn.gordon20@gmail.com>
2025-06-17 09:19:33 -0500

    neotree, terminal keymaps

--- init.lua
+++ init.lua
@@ -205,6 +205,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- [[ Custom Keymaps ]]
+vim.keymap.set('n', '<leader>tt', '<cmd>tabnew<CR><cmd>term<CR>')
+vim.keymap.set('n', '<leader>ts', '<cmd>split<CR><cmd>term<CR>')
+vim.keymap.set('n', '<leader>tv', '<cmd>vsplit<CR><cmd>term<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -980,7 +985,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
