cf2ca089eeee453a04bb4f37eb9da4e29d2cfdd1
Evelyn Gordon <evelyn.gordon20@gmail.com>
2026-02-09 09:56:42 -0600

    LSP for tfbackend and tfvars files

--- init.lua
+++ init.lua
@@ -230,6 +230,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Manual filetype associations
+vim.filetype.add {
+  extension = {
+    tfbackend = 'terraform-vars',
+    tfvars = 'terraform-vars',
+  },
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
